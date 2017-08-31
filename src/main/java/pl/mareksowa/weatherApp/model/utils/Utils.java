7d4974fae48659f188952aa43b3abe58be488cb2
Kerawos <Maresowa@gmail.com>
2017-08-31 20:50:32 +0200

    //comments update3

--- src/main/java/pl/mareksowa/weatherApp/model/utils/Utils.java
+++ src/main/java/pl/mareksowa/weatherApp/model/utils/Utils.java
@@ -1,7 +1,6 @@
 package pl.mareksowa.weatherApp.model.utils;
 
 import javafx.application.Platform;
-import pl.mareksowa.weatherApp.model.serice.WeatherService;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,30 +10,28 @@ import java.net.URL;
 
 public class Utils { //Utils contains common methods
 
-    public static String readWebsideContext(String url){
+    public static String readWebsiteContext(String url){
         StringBuilder builder = new StringBuilder();
 
         try {
-            HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();//rzuujemy odrazu na http
+            HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();//cast to HTTP
             InputStream inputStream = httpURLConnection.getInputStream();
-
             int response;
-            while ((response = inputStream.read()) != -1){ // read zwraca -1 gdy nie ma co juz czytac (response ma szybka inicjalizacje)
+            while ((response = inputStream.read()) != -1){ // back read field
                 builder.append((char)response);
             }
-
             inputStream.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return builder.toString(); //wracamy to co sobie utworzylismy jako string
+        return builder.toString(); //return our build string
     }
 
 
     public static void closeApp(){
-        WeatherService.terminateExecutorService(); //before close terminate all threads
         Platform.exit();
+        System.out.println("App closing..");
+        System.exit(0);
     }
 
 }

6e2c0ec0dac9ccf077b6ae73594831e9323e23f8
Kerawos <Maresowa@gmail.com>
2017-09-04 11:59:20 +0200

    //comments update4 - password hidding

--- src/main/java/pl/mareksowa/weatherApp/Config.java
+++ src/main/java/pl/mareksowa/weatherApp/Config.java
@@ -1,6 +1,12 @@
 package pl.mareksowa.weatherApp;
 
-public class Config { // trzymamy sobie nasze zmienne statyczne do ktorcyh mozemy sie dowolac z roznych miejsc
+//holder for global unchangable variables
+public class Config {
     public static final String APP_KEY = "c4ec9252ef85b0d67139c25bc342411f";
     public static final String API_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    //statics
+    public static final String SQL_LINK = "jdbc:mysql://5.135.218.27:3306/Marek?characterEncoding=utf8";
+    public static final String SQL_USER = "M***k";
+    public static final String SQL_PASS = "K***1";
+    public static final String SQL_CLASS = "com.mysql.jdbc.Driver";
 }
